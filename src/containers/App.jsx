6cd605136fc9f53346b4a2fae6af355ab5838301
illich <illich570@gmail.com>
2021-02-09 17:52:13 -0400

    [ADD] added prop-types. fetching from json-server and dynamic render.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,20 +5,42 @@ import '../assets/styles/App.scss';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import useInitialState from '../hooks/useInitialState';
 
 const App = () => {
+
+  const [videos, categories] = useInitialState('http://localhost:3000/initialState');
+
   return (
     <>
       <Header />
       <Search />
-      <Categories>
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {
+        categories.length > 0 && (
+          categories.map((category, index) => (
+            <>
+              { videos[category].length > 0 && (
+                <Categories title={category} key={`index_${index + 10}`}>
+                  <Carousel>
+                    {
+                      videos[category].map((item, index) => (
+                        <CarouselItem
+                          key={`in_${index + 1}`}
+                          cover={item.cover}
+                          title={item.title}
+                          year={item.year}
+                          contentRating={item.contentRating}
+                          duration={item.duration}
+                        />
+                      ))
+                    }
+                  </Carousel>
+                </Categories>
+              )}
+            </>
+          ))
+        )
+      }
     </>
   );
 };
